806cfaeb98d80f47546bd2984ffb96a455a1c0d1
phamthanhx18 <138504962+phamthanhx18@users.noreply.github.com>
2023-11-16 14:05:07 +0700

    fix btap

--- ss02_reactjs_jsx/exercise/student-of-class/src/components/Student.js
+++ ss02_reactjs_jsx/exercise/student-of-class/src/components/Student.js
@@ -1,46 +1,51 @@
 import React, {Component} from "react";
 
-class Student extends Component{
+const studentList = [
+    {
+        company: 'Alfreds Futterkiste',
+        contact: 'Maria Anders',
+        country: 'Germany'
+    },
+    {
+        company: 'Centro comercial Moctezuma',
+        contact: 'Francisco Chang',
+        country: 'Mexico'
+    },
+    {
+        company: 'Ernst Handel',
+        contact: 'Roland Mendel',
+        country: 'Austria'
+    },
+    {
+        company: 'Island Trading',
+        contact: 'Helen Bennett',
+        country: 'UK'
+    },
+    {
+        company: 'Laughing Bacchus Winecellars',
+        contact: 'Yoshi Tannamuri',
+        country: 'Canada'
+    },
+    {
+        company: 'Magazzini Alimentari Riuniti',
+        contact: 'Giovanni Rovelli',
+        country: 'Italy'
+    }
+];
+
+class Student extends Component {
     constructor(props) {
         super(props);
-        this.state = [
-            {
-                company: 'Alfreds Futterkiste',
-                contact: 'Maria Anders',
-                country: 'Germany'
-            },
-            {
-                company: 'Centro comercial Moctezuma',
-                contact: 'Francisco Chang',
-                country: 'Mexico'
-            },
-            {
-                company: 'Ernst Handel',
-                contact: 'Roland Mendel',
-                country: 'Austria'
-            },
-            {
-                company: 'Island Trading',
-                contact: 'Helen Bennett',
-                country: 'UK'
-            },
-            {
-                company: 'Laughing Bacchus Winecellars',
-                contact: 'Yoshi Tannamuri',
-                country: 'Canada'
-            },
-            {
-                company: 'Magazzini Alimentari Riuniti',
-                contact: 'Giovanni Rovelli',
-                country: 'Italy'
-            }
-        ];
+        this.state = {
+            studentList: studentList
+        };
     }
+
     render() {
         return <>
             <table>
                 {
-                    this.state.map(student => (
+                    this.state.studentList.map(student => (
                         <tr>
                             <td>{student.company}</td>
                             <td>{student.contact}</td>
