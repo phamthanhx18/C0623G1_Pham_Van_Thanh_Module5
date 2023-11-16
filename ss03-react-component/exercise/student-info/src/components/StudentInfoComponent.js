61288255d574bbbe21dd33bd2d5ec6686893a88f
phamthanhx18 <138504962+phamthanhx18@users.noreply.github.com>
2023-11-16 15:09:32 +0700

    thanh up btap ss03

---
+++ ss03-react-component/exercise/student-info/src/components/StudentInfoComponent.js
@@ -0,0 +1,54 @@
+import {Component} from "react";
+const listStudent = [
+    {
+        id: 1,
+        name: "ThanhPV",
+        age: "23",
+        address: "Nghe An"
+    },
+    {
+        id: 2,
+        name: "HaiTT",
+        age: "25",
+        address: "Quang Nam"
+    },
+    {
+        id: 3,
+        name: "PhuongTD",
+        age: "25",
+        address: "Da Nang"
+    }
+];
+
+class StudentInfoComponent extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            listStudent: listStudent
+        }
+    }
+    render() {
+        return <>
+            <table>
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Tên</th>
+                <th>Tuổi</th>
+                <th>Địa chỉ</th>
+            </tr>
+            </thead>
+            <tbody>
+            {this.state.listStudent.map(student => (
+                <tr>
+                    <td>{student.id}</td>
+                    <td>{student.name}</td>
+                    <td>{student.age}</td>
+                    <td>{student.address}</td>
+                </tr>
+            ))}
+            </tbody>
+        </table></>;
+    }
+}
+export default StudentInfoComponent;
