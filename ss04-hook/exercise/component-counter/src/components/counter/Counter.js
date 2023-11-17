774e5b48f9ca5702ab30411a39c4d2d1ef122333
phamthanhx18 <138504962+phamthanhx18@users.noreply.github.com>
2023-11-17 16:31:41 +0700

    up btap ss04

---
+++ ss04-hook/exercise/component-counter/src/components/counter/Counter.js
@@ -0,0 +1,21 @@
+import {useState} from "react";
+
+function Counter() {
+    const [numberValue1, setNumberValue1] = useState(0);
+    const [numberValue2, setNumberValue2] = useState(0);
+    const incrementCount1 = () => {
+        setNumberValue1(numberValue1 => numberValue1 + 1)
+    }
+    const incrementCount2 = () => {
+        setNumberValue2(numberValue2 => numberValue2 + 2)
+    }
+    return (
+        <>
+            <p>Count : {numberValue1}</p>
+            <button onClick={incrementCount1}>Add 1</button>
+            <p>Count : {numberValue2}</p>
+            <button onClick={incrementCount2}>Add 2</button>
+        </>
+    )
+}
+export default Counter;
