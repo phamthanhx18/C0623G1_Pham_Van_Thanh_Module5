52f8f081a991daa6ea4d8260f7a74c0349a92297
phamthanhx18 <phamthanhx18@gmail.com>
2023-11-29 23:00:18 +0700

    up

---
+++ finally_md_5/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+    return (
+        <>
+            <h2>Hello world !</h2>
+        </>
+    );
+}
+
+export default App;
